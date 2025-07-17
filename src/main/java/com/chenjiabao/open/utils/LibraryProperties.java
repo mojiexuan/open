27e22bc6dce427f3bd688b06737303968b8f95db
陈佳宝 <mail@chenjiabao.com>
2025-07-17 16:56:44 +0800

    0.4.1

--- src/main/java/com/chenjiabao/open/utils/LibraryProperties.java
+++ src/main/java/com/chenjiabao/open/utils/LibraryProperties.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
 /**
  * @author ChenJiaBao
@@ -17,17 +18,22 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "chenjiabao.config")
 public class LibraryProperties {
     // 分布式机器信息
+    @NestedConfigurationProperty
     private Machine machine = new Machine();
 
     // 胡椒值
+    @NestedConfigurationProperty
     private Hash hash = new Hash();
 
     // 邮件配置
+    @NestedConfigurationProperty
     private Mail mail = new Mail();
 
     // JWT配置
+    @NestedConfigurationProperty
     private Jwt jwt = new Jwt();
 
     // 文件上传配置
+    @NestedConfigurationProperty
     private File file = new File();
 }
