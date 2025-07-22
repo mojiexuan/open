e6ece202b404046d11951ff785ab24a8336d8466
陈佳宝 <mail@chenjiabao.com>
2025-07-22 17:37:35 +0800

    0.4.8

--- src/main/java/com/chenjiabao/open/utils/FilesUtils.java
+++ src/main/java/com/chenjiabao/open/utils/FilesUtils.java
@@ -4,12 +4,14 @@ import com.chenjiabao.open.utils.enums.RequestCode;
 import com.chenjiabao.open.utils.model.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.*;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
 /**
  * 文件类
+ *
  * @author ChenJiaBao
  */
 public class FilesUtils {
@@ -30,19 +32,20 @@ public class FilesUtils {
     /**
      * 创建文件目录及父目录
      */
-    public boolean createDirectory(File directory){
-        if(!isHasFile(directory)){
+    public boolean createDirectory(File directory) {
+        if (!isHasFile(directory)) {
             return directory.mkdirs();
-        }else {
+        } else {
             return true;
         }
     }
 
     /**
      * 判断文件是否存在
+     *
      * @param directory 文件目录
      */
-    public boolean isHasFile(File directory){
+    public boolean isHasFile(File directory) {
         //exists()方法可以校验是否存在该文件，防止抛出异常
         return directory.exists();
     }
@@ -50,17 +53,17 @@ public class FilesUtils {
     /**
      * 创建文件
      */
-    public boolean createFile(File file){
+    public boolean createFile(File file) {
         boolean flag;
-        try{
+        try {
             //创建父目录，若存在
             File parentFile = file.getParentFile();
-            if(!parentFile.exists()){
+            if (!parentFile.exists()) {
                 createDirectory(parentFile);
             }
             //创建文件
             flag = file.createNewFile();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             flag = false;
         }
@@ -71,14 +74,14 @@ public class FilesUtils {
     /**
      * 删除文件
      */
-    public boolean deleteFile(File file){
+    public boolean deleteFile(File file) {
         return file.delete();
     }
 
     /**
      * 字符流 缓冲流 读取文件
      */
-    public ArrayList<String> readerFile(String file){
+    public ArrayList<String> readerFile(String file) {
 
         ArrayList<String> arrayList = new ArrayList<>();
         arrayList.add("999");
@@ -86,17 +89,17 @@ public class FilesUtils {
         Reader fr = null;
         BufferedReader br = null;
 
-        try{
+        try {
             fr = new FileReader(file);
             br = new BufferedReader(fr);//构造方法需要一个FileReader对象
 
             String data = null;
-            while ((data = br.readLine()) != null){
+            while ((data = br.readLine()) != null) {
                 //readLine()方法可以一行一行地读取
                 arrayList.add(data);
             }
         } catch (IOException e) {
-            arrayList.set(0,"-1");//获取失败！
+            arrayList.set(0, "-1");//获取失败！
             e.printStackTrace();
         } finally {
             //关闭流
@@ -109,14 +112,15 @@ public class FilesUtils {
 
     /**
      * 写入文件
+     *
      * @param append 是否开启追加模式
      */
-    public boolean writerFile(String file,String content,boolean append){
+    public boolean writerFile(String file, String content, boolean append) {
         FileWriter fw = null;
         BufferedWriter bw = null;
 
-        try{
-            fw = new FileWriter(file,append);
+        try {
+            fw = new FileWriter(file, append);
             bw = new BufferedWriter(fw);
 
             bw.write(content);
@@ -125,59 +129,62 @@ public class FilesUtils {
         } catch (IOException e) {
             e.printStackTrace();
             return false;
-        }finally {
+        } finally {
             closeFile(bw);
             closeFile(fw);
         }
     }
 
     /**
-     *关闭流
+     * 关闭流
      */
-    public boolean closeFile(Reader fileReader){
-        if(fileReader == null){
+    public boolean closeFile(Reader fileReader) {
+        if (fileReader == null) {
             return true;
         }
         try {
             fileReader.close();
             return true;
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
     }
-    public boolean closeFile(BufferedReader bufferedReader){
-        if(bufferedReader == null){
+
+    public boolean closeFile(BufferedReader bufferedReader) {
+        if (bufferedReader == null) {
             return true;
         }
         try {
             bufferedReader.close();
             return true;
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
     }
-    public boolean closeFile(FileWriter fileWriter){
-        if(fileWriter == null){
+
+    public boolean closeFile(FileWriter fileWriter) {
+        if (fileWriter == null) {
             return true;
         }
         try {
             fileWriter.close();
             return true;
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
     }
-    public boolean closeFile(BufferedWriter bufferedWriter){
-        if(bufferedWriter == null){
+
+    public boolean closeFile(BufferedWriter bufferedWriter) {
+        if (bufferedWriter == null) {
             return true;
         }
         try {
             bufferedWriter.close();
             return true;
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
@@ -185,10 +192,11 @@ public class FilesUtils {
 
     /**
      * 检查文件是否合法
+     *
      * @param file 文件
      * @return ApiResponse
      */
-    public ApiResponse checkFile(MultipartFile file){
+    public ApiResponse checkFile(MultipartFile file) {
         if (file.isEmpty()) {
             return ApiResponse.error(RequestCode.CODE_406, "文件为空").build();
         }
@@ -209,7 +217,7 @@ public class FilesUtils {
 
         long size = file.getSize();
         // 10MB
-        if(size > properties.getFile().getMaxSize()){
+        if (size > properties.getFile().getMaxSize()) {
             return ApiResponse.error(RequestCode.CODE_406, "文件过大").build();
         }
 
@@ -218,9 +226,12 @@ public class FilesUtils {
 
     /**
      * 保存文件
+     *
+     * @param file 文件
+     * @param savePath 保存路径,需要前导/，不要尾随/，如：/20200301
      * @return null为失败，成功则为路径
      */
-    public String saveFile(MultipartFile file){
+    public String saveFile(MultipartFile file, String savePath) {
         String fileName = file.getOriginalFilename();
         if (fileName == null || !fileName.contains(".")) {
             return null;
@@ -236,23 +247,32 @@ public class FilesUtils {
         // 拼接新的文件名
         newFileName = newFileName + randomNumber + fileSuffix;
 
-        String path = properties.getFile().getPath() + "/" + dir + "/";
+        String path = savePath + "/" + dir + "/";
 
         //自动创建父目录，若不存在
-        if(!createDirectory(new File(classesPath + path))){
+        if (!createDirectory(new File(classesPath + path))) {
             return null;
         }
 
         // 拼接完整文件名
         String allPath = classesPath + path + newFileName;
         // 将文件放进指定目录
-        try{
+        try {
             file.transferTo(new File(allPath));
         } catch (IOException e) {
             return null;
         }
 
         return path + newFileName;
+    }
+
+    /**
+     * 保存文件
+     *
+     * @return null为失败，成功则为路径
+     */
+    public String saveFile(MultipartFile file) {
+        return this.saveFile(file,properties.getFile().getPath());
     }
 
 }
